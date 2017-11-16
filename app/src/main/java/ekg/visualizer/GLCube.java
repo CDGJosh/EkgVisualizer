3e803c4c43d28ce300e8c3d37bc13c77b4fd1657
Josh <cdg.josh@gmail.com>
2017-11-16 11:42:29 +0100

    new indices, fixing cull face

--- app/src/main/java/ekg/visualizer/GLCube.java
+++ app/src/main/java/ekg/visualizer/GLCube.java
@@ -26,50 +26,84 @@ public class GLCube {
     private final String cubeVSSrc =
                     "attribute vec3 vertex;" +
                     "uniform vec3 position;"+
+                    "uniform vec3 center;"+
+                    "uniform vec3 rotation;"+
                     "uniform float scale;"+
-                            "uniform vec4 color;" +
+                    "uniform float zoom;"+
                     "void main() {" +
-                            "mat4 trans = mat4(1.0, 0.0, 0.0, position.x,    0.0, 1.0, 0.0, position.y,    0.0, 0.0, 1.0, position.z,    0.0, 0.0, 0.0, 1.0);"+
-                    "  gl_Position = trans * vec4(vertex.x, vertex.y, vertex.z, 1.0);" +
+                            "float yang = 0.0;"+//785;"+
+                            "float xang = 0.0;"+
+                            "mat4 trans = mat4(1.0,     0.0, 0.0, 0.0,    0.0, 1.0,   0.0, 0.0,    0.0, 0.0, 1.0,   0.0,    position.x - center.x, position.y - center.y, position.z - center.z, 1.0);"+
+                            "mat4 scaling = mat4(scale, 0.0, 0.0, 0.0,    0.0, scale, 0.0, 0.0,    0.0, 0.0, scale, 0.0,    0.0,        0.0,        0.0,        1.0);"+
+                            "mat4 z = mat4(zoom,     0.0, 0.0, 0.0,    0.0, zoom,   0.0, 0.0,    0.0, 0.0, zoom,   0.0,    0.0,        0.0,       0.0,        1.0);"+
+                            "mat4 yrot =    mat4( cos(rotation.y), 0.0, - sin(rotation.y), 0.0,    0.0, 1.0, 0.0, 0.0,    sin(rotation.y), 0.0, cos(rotation.y), 0.0,    0.0, 0.0, 0.0, 1.0);"+
+                            "mat4 xrot =    mat4( 1.0, 0.0, 0.0, 0.0,    0.0, cos(rotation.x), sin(rotation.x), 0.0,    0.0, - sin(rotation.x), cos(rotation.x), 0.0,    0.0, 0.0, 0.0, 1.0);"+
+                            "mat4 rot = xrot * yrot;"+
+                    "  gl_Position =  z* rot * trans  * scaling *vec4(vertex.x, vertex.y, vertex.z, 1.0);" +
                     "}";
 
     private final String cubeFSSrc  =
             "precision mediump float;" +
                     "uniform vec4 color;" +
                     "void main() {" +
-                    "  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);" +
+                    "  gl_FragColor = color;" +
                     "}";
 
     private int vertexVBO;
     private int indicesVBO;
+    private int outlineIndicesVBO;
+    private int wireframeIndicesVBO;
 
     private static GLCube instance;
     public GLCube()
     {
 
         float[] cubeVertex = new float[]{
-                1.0f, 1.0f, 1.0f,    //front top right  0
-               -1.0f, 1.0f, 1.0f,    //front top left   1
-               -1.0f, -1.0f, 1.0f,   //front bot left   2
-                1.0f, -1.0f, 1.0f,   //front bot right  3
-                1.0f, 1.0f, -1.0f,   //back top right   4
-                -1.0f, 1.0f, -1.0f,  //back top left    5
-                -1.0f, -1.0f, -1.0f, //back bot left    6
-                1.0f, -1.0f, -1.0f   //back bot right   7
+                -1.0f, -1.0f, 1.0f,  //Bottom Left   0
+                 1.0f, -1.0f, 1.0f,  //Bottom Right  1
+                 1.0f,  1.0f, 1.0f,  //Top Right     2
+                -1.0f,  1.0f, 1.0f,  //Top left      3
+
+                -1.0f, -1.0f, -1.0f, //Bottom Left   4
+                 1.0f, -1.0f, -1.0f, //Bottom Right  5
+                 1.0f,  1.0f, -1.0f, //Top Right     6
+                -1.0f,  1.0f, -1.0f  //Top left      7
         };
 
         byte[] cubeIndices = new byte[]{
-                0, 1, 2, 2, 3, 0, //front
-                0, 1, 4, 4, 5, 0, //top
-                1, 5, 6, 6, 2, 1, //left
-                0, 4, 7, 7, 3, 0, //right
-                2, 6, 7, 7, 3, 2, //bot
-                4, 5, 6, 6, 7, 4  //back
+                0,1,2, 0,2,3,//front
+                0,3,7, 0,7,4,//Left
+                5,1,0, 4,5,0,//Bottom
+
+                4,7,6, 5,4,6,//Back
+                6,7,3, 6,3,2,//top
+                6,2,1, 6,1,5//right
         };
-        int[] vbos = new int[2];
-        glGenBuffers(2,  vbos, 0);
+
+        byte[] outlineIndices = new byte[]{
+                0,1, 1,2, 2,3, 3,0, //front
+                0,3, 3,7, 7,4, 4,0,//Left
+                5,1, 1,0, 0,4, 4,5,//Bottom
+
+                4,7, 7,6, 6,5, 5,4,//Back
+                6,7, 7,3, 3,2, 2,6,//top
+                6,2, 2,1, 1,5, 5,6//right
+        };
+
+        byte[] wireframeIndices = new byte[]{
+                0, 1, 1, 2, 2, 0, 2, 3, 3, 0, //front
+                0, 1, 1, 4, 4, 0, 4, 5, 5, 1, //top
+                1, 5, 5, 6, 6, 1, 6, 2, 2, 1, //left
+                0, 4, 4, 7, 7, 0, 7, 3, 3, 0, //right
+                2, 6, 6, 7, 7, 2, 7, 3, 3, 2, //bot
+                4, 5, 5, 6, 6, 4, 6, 7, 7, 4  //back
+        };
+        int[] vbos = new int[4];
+        glGenBuffers(4,  vbos, 0);
         this.vertexVBO = vbos[0];
         this.indicesVBO = vbos[1];
+        this.outlineIndicesVBO = vbos[2];
+        this.wireframeIndicesVBO = vbos[3];
 
         ByteBuffer vertBB = ByteBuffer.allocateDirect((cubeVertex.length * 4)); // a float is 4 bytes
         vertBB.order(ByteOrder.nativeOrder()); // use native order for bytes
@@ -85,6 +119,20 @@ public class GLCube {
         indicesBuffer.position(0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.indicesVBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, cubeIndices.length, indicesBuffer, GL_STATIC_DRAW);
+
+        ByteBuffer outlineIndicesBuffer = ByteBuffer.allocateDirect((outlineIndices.length));
+        outlineIndicesBuffer.order(ByteOrder.nativeOrder());
+        outlineIndicesBuffer.put(outlineIndices);
+        outlineIndicesBuffer.position(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.outlineIndicesVBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, outlineIndices.length, outlineIndicesBuffer, GL_STATIC_DRAW);
+
+        ByteBuffer wireframeIndicesBuffer = ByteBuffer.allocateDirect((wireframeIndices.length));
+        wireframeIndicesBuffer.order(ByteOrder.nativeOrder());
+        wireframeIndicesBuffer.put(wireframeIndices);
+        wireframeIndicesBuffer.position(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.wireframeIndicesVBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, wireframeIndices.length, wireframeIndicesBuffer, GL_STATIC_DRAW);
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -150,16 +198,15 @@ public class GLCube {
     private int positionUniformLocation = -1;
     private int scaleUniformLocation = -1;
 
+    private int centerUniformLocation = -1;
+    private int rotationUniformLocation = -1;
+
     private int vertexAttribLocation = -1;
+    private int zoomAttribLocation = -1;
 
     private static final int POINTS_PER_VERTEX = 3;
 
-    public void drawAt(float x, float y, float z, float scale)
-    {
-        this.drawAt(x,y,z,0,0,0,0,0,0,scale);
-    }
-
-    public void drawAt(float x, float y, float z, float xrot, float yrot, float zrot, float cx, float cy, float cz, float scale)
+    public void drawAt(float x, float y, float z, float xrot, float yrot, float zrot, float cx, float cy, float cz, float scale, float zoom)
     {
 
         glUseProgram(this.shader);
@@ -174,21 +221,66 @@ public class GLCube {
         if(this.scaleUniformLocation == -1)
             this.scaleUniformLocation = glGetUniformLocation(this.shader, "scale");
 
+        if(this.centerUniformLocation == -1)
+            this.centerUniformLocation = glGetUniformLocation(this.shader, "center");
+
+        if(this.rotationUniformLocation == -1)
+            this.rotationUniformLocation = glGetUniformLocation(this.shader, "rotation");
+
+        if(this.zoomAttribLocation == -1)
+            this.zoomAttribLocation = glGetUniformLocation(this.shader, "zoom");
+
         if(this.vertexAttribLocation == -1)
             this.vertexAttribLocation = glGetAttribLocation(this.shader,"vertex");
 
 
         glUniform4f(this.colorUniformLocation, 1.0f, 1.0f, 1.0f, 0.8f);
         glUniform3f(this.positionUniformLocation, x,y,z);
+
+        glUniform3f(this.rotationUniformLocation, xrot,yrot,zrot);
+
+        glUniform3f(this.centerUniformLocation, cx,cy,cz);
         glUniform1f(this.scaleUniformLocation, scale);
+        glUniform1f(this.zoomAttribLocation, zoom);
+
 
         glBindBuffer(GL_ARRAY_BUFFER, this.vertexVBO);
         glEnableVertexAttribArray(this.vertexAttribLocation);
         glVertexAttribPointer(this.vertexAttribLocation, POINTS_PER_VERTEX, GL_FLOAT, false, 0,0);
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.indicesVBO);
+        glDepthFunc((GL_ALWAYS));
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.indicesVBO);
+        /*
+        glUniform4f(this.colorUniformLocation, 1.0f, 0.0f, 0.0f, 0.6f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0); // FRONT
+
+        glUniform4f(this.colorUniformLocation, 0.0f, 1.0f, 0.0f, 0.6f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 6); // LEFT
+
+        glUniform4f(this.colorUniformLocation, 0.0f, 0.0f, 1.0f, 0.6f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 12); // BOT
+
+        glUniform4f(this.colorUniformLocation, 1.0f, 1.0f, 0.0f, 0.6f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 18); // BACK
+
+        glUniform4f(this.colorUniformLocation, 1.0f, 0.0f, 1.0f, 0.6f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 24); // TOP
+
+        glUniform4f(this.colorUniformLocation, 0.0f, 1.0f, 1.0f, 0.6f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 30); // RIGHT
+        */
+        glUniform4f(this.colorUniformLocation, 1.0f, 1.0f, 1.0f, 0.8f);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, 0);
+        glUniform4f(this.colorUniformLocation, 0.0f, 0.0f, 0.0f, 1.0f);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.outlineIndicesVBO);
+        glLineWidth(100.0f * scale);
+        glDepthFunc( GL_GEQUAL);
+        glDrawElements(GL_LINES, 48, GL_UNSIGNED_BYTE, 0);
+
+        //glUniform1f(this.scaleUniformLocation, scale + 0.001f);
+        //glDrawArrays(GL_LINES, 0, 36);
 
 
     }
